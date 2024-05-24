4491dede61069f4ea99d18d4ef19845bbe321d5a
Nick Read <read1762@gmail.com>
2024-05-24 11:27:25 -0500

    token

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def box_webhook():
             # Update metadata with AI insights
 
             AIresponse = requests.get(url=f"https://api.box.com/2.0/metadata_instances/suggestions?item=file_{file_id}&scope=enterprise_964447513&template_key=aitest&confidence=experimental"
-                        , headers={"Authorization": "Bearer 1YwFRwFaHKEktTs1hlEtsXqxRQm129QO"})
+                        , headers={"Authorization": "Bearer EwsQ56X0HvLMhrfis4VBulvT5XlTEx9M"})
             print(AIresponse.text)
             
             if AIresponse.status_code == 200:
