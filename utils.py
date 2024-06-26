c7381d14b78b40f66c8c4d066bc559ae0e8fc204
Nick Read <read1762@gmail.com>
2024-06-26 08:21:20 -0500

    taking out the contract AI

--- utils.py
+++ utils.py
@@ -374,11 +374,12 @@ def extract_nike_contract_ai_attributes(suggestions, schema):
 
 # Mapping of template keys to extraction functions
 template_extractors = {
-    "contractAi": extract_contract_ai_attributes,
+    # Add more mappings for other templates
+    #"contractAi": extract_contract_ai_attributes,
     "aitest": extract_order_form_ai_attributes,
     #"uberaiextract": extract_uber_ai_attributes,
     "nikeplayercontrat": extract_nike_contract_ai_attributes,
     #"nikeallsportsagreement": extract_nike_all_sports_agreement_attributes,
-    # Add more mappings for other templates
+    
 }
 
